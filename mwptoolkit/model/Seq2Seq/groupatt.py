28a6c5a37c80f887b8e7579e7f020630d5e93241
LYH-YF <yifan2250@gmail.com>
2021-05-14 08:52:03 +0800

    groupatt

--- mwptoolkit/model/Seq2Seq/groupatt.py
+++ mwptoolkit/model/Seq2Seq/groupatt.py
@@ -85,59 +85,6 @@ class GroupATT(nn.Module):
             all_outputs=self.generate_without_t(encoder_outputs,encoder_hidden,decoder_inputs)
             return all_outputs
 
-    def generate_t(self,encoder_outputs,encoder_hidden,decoder_inputs):
-        with_t=random.random()
-        seq_len=decoder_inputs.size(1)
-        decoder_hidden = encoder_hidden
-        decoder_input = decoder_inputs[:,0,:].unsqueeze(1)
-        token_logits=[]
-        output=[]
-        for idx in range(seq_len):
-            if with_t<self.teacher_force_ratio:
-                decoder_input = decoder_inputs[:,idx,:].unsqueeze(1)
-            if self.attention:
-                decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden,encoder_outputs)
-            else:
-                decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden)
-            #attn_list.append(attn)
-            step_output = decoder_output.squeeze(1)
-            token_logit = self.generate_linear(step_output)
-            predict=torch.nn.functional.log_softmax(token_logit,dim=1)
-            output=self.rule_filter_(output,token_logit)
-            token_logits.append(predict)
-
-            if self.share_vocab:
-                output_=self.decode(output)
-                decoder_input=self.out_embedder(output_)
-            else:
-                decoder_input=self.out_embedder(output)
-        token_logits=torch.stack(token_logits,dim=1)
-        token_logits=token_logits.view(-1,token_logits.size(-1))
-        return token_logits
-    
-    def generate_without_t(self,encoder_outputs,encoder_hidden,decoder_input):
-        all_outputs=[]
-        decoder_hidden = encoder_hidden
-        output=[]
-        for idx in range(self.max_gen_len):
-            if self.attention:
-                decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden,encoder_outputs)
-            else:
-                decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden)
-            
-            step_output = decoder_output.squeeze(1)
-            token_logits = self.generate_linear(step_output)
-            output = self.rule_filter_(output,token_logits)
-            
-            all_outputs.append(output)
-            if self.share_vocab:
-                output_=self.decode(output)
-                decoder_input=self.out_embedder(output_)
-            else:
-                decoder_input=self.out_embedder(output)
-        all_outputs=torch.cat(all_outputs,dim=1)
-        return all_outputs
-    
     def init_decoder_inputs(self,target,device,batch_size):
         pad_var = torch.LongTensor([self.out_sos_token]*batch_size).to(device).view(batch_size,1)
         if target != None:
@@ -305,63 +252,116 @@ class GroupATT(nn.Module):
         parameters="\ntotal parameters : {} \ntrainable parameters : {}".format(total,trainable)
         return info+parameters
     
+    def generate_t(self,encoder_outputs,encoder_hidden,decoder_inputs):
+        with_t=random.random()
+        if with_t<self.teacher_force_ratio:
+            if self.attention:
+                decoder_outputs, decoder_hidden = self.decoder(decoder_inputs, encoder_hidden,encoder_outputs)
+            else:
+                decoder_outputs, decoder_hidden = self.decoder(decoder_inputs, encoder_hidden)
+            token_logits = self.generate_linear(decoder_outputs)
+            token_logits=token_logits.view(-1, token_logits.size(-1))
+            token_logits=torch.nn.functional.log_softmax(token_logits,dim=1)
+            # token_logits=torch.log_softmax(token_logits,dim=1)
+        else:
+            seq_len=decoder_inputs.size(1)
+            decoder_hidden = encoder_hidden
+            decoder_input = decoder_inputs[:,0,:].unsqueeze(1)
+            token_logits=[]
+            for idx in range(seq_len):
+                if self.attention:
+                    decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden,encoder_outputs)
+                else:
+                    decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden)
+                #attn_list.append(attn)
+                step_output = decoder_output.squeeze(1)
+                token_logit = self.generate_linear(step_output)
+                predict=torch.nn.functional.log_softmax(token_logit,dim=1)
+                #predict=torch.log_softmax(token_logit,dim=1)
+                output=predict.topk(1,dim=1)[1]
+                token_logits.append(predict)
+
+                if self.share_vocab:
+                    output=self.decode(output)
+                    decoder_input=self.out_embedder(output)
+                else:
+                    decoder_input=self.out_embedder(output)
+            token_logits=torch.stack(token_logits,dim=1)
+            token_logits=token_logits.view(-1,token_logits.size(-1))
+        return token_logits
+
+    def generate_without_t(self,encoder_outputs,encoder_hidden,decoder_input):
+        all_outputs=[]
+        decoder_hidden = encoder_hidden
+        for idx in range(self.max_gen_len):
+            if self.attention:
+                decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden,encoder_outputs)
+            else:
+                decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden)
+            #attn_list.append(attn)
+            step_output = decoder_output.squeeze(1)
+            token_logits = self.generate_linear(step_output)
+            predict=torch.nn.functional.log_softmax(token_logits,dim=1)
+            output=predict.topk(1,dim=1)[1]
+            
+            
+            all_outputs.append(output)
+            if self.share_vocab:
+                output=self.decode(output)
+                decoder_input=self.out_embedder(output)
+            else:
+                decoder_input=self.out_embedder(output)
+        all_outputs=torch.cat(all_outputs,dim=1)
+        return all_outputs
+    
     # def generate_t(self,encoder_outputs,encoder_hidden,decoder_inputs):
     #     with_t=random.random()
-    #     if with_t<self.teacher_force_ratio:
-    #         if self.attention:
-    #             decoder_outputs, decoder_hidden = self.decoder(decoder_inputs, encoder_hidden,encoder_outputs)
-    #         else:
-    #             decoder_outputs, decoder_hidden = self.decoder(decoder_inputs, encoder_hidden)
-    #         token_logits = self.generate_linear(decoder_outputs)
-    #         token_logits=token_logits.view(-1, token_logits.size(-1))
-    #         token_logits=torch.nn.functional.log_softmax(token_logits,dim=1)
-    #         # token_logits=torch.log_softmax(token_logits,dim=1)
-    #     else:
-    #         seq_len=decoder_inputs.size(1)
-    #         decoder_hidden = encoder_hidden
-    #         decoder_input = decoder_inputs[:,0,:].unsqueeze(1)
-    #         token_logits=[]
-    #         for idx in range(seq_len):
-    #             if self.attention:
-    #                 decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden,encoder_outputs)
-    #             else:
-    #                 decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden)
-    #             #attn_list.append(attn)
-    #             step_output = decoder_output.squeeze(1)
-    #             token_logit = self.generate_linear(step_output)
-    #             predict=torch.nn.functional.log_softmax(token_logit,dim=1)
-    #             #predict=torch.log_softmax(token_logit,dim=1)
-    #             output=predict.topk(1,dim=1)[1]
-    #             token_logits.append(predict)
-
-    #             if self.share_vocab:
-    #                 output=self.decode(output)
-    #                 decoder_input=self.out_embedder(output)
-    #             else:
-    #                 decoder_input=self.out_embedder(output)
-    #         token_logits=torch.stack(token_logits,dim=1)
-    #         token_logits=token_logits.view(-1,token_logits.size(-1))
-    #     return token_logits
-
-    # def generate_without_t(self,encoder_outputs,encoder_hidden,decoder_input):
-    #     all_outputs=[]
+    #     seq_len=decoder_inputs.size(1)
     #     decoder_hidden = encoder_hidden
-    #     for idx in range(self.max_gen_len):
+    #     decoder_input = decoder_inputs[:,0,:].unsqueeze(1)
+    #     token_logits=[]
+    #     output=[]
+    #     for idx in range(seq_len):
+    #         if with_t<self.teacher_force_ratio:
+    #             decoder_input = decoder_inputs[:,idx,:].unsqueeze(1)
     #         if self.attention:
     #             decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden,encoder_outputs)
     #         else:
     #             decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden)
     #         #attn_list.append(attn)
     #         step_output = decoder_output.squeeze(1)
-    #         token_logits = self.generate_linear(step_output)
-    #         predict=torch.nn.functional.log_softmax(token_logits,dim=1)
-    #         output=predict.topk(1,dim=1)[1]
+    #         token_logit = self.generate_linear(step_output)
+    #         predict=torch.nn.functional.log_softmax(token_logit,dim=1)
+    #         output=self.rule_filter_(output,token_logit)
+    #         token_logits.append(predict)
+
+    #         if self.share_vocab:
+    #             output_=self.decode(output)
+    #             decoder_input=self.out_embedder(output_)
+    #         else:
+    #             decoder_input=self.out_embedder(output)
+    #     token_logits=torch.stack(token_logits,dim=1)
+    #     token_logits=token_logits.view(-1,token_logits.size(-1))
+    #     return token_logits
+    
+    # def generate_without_t(self,encoder_outputs,encoder_hidden,decoder_input):
+    #     all_outputs=[]
+    #     decoder_hidden = encoder_hidden
+    #     output=[]
+    #     for idx in range(self.max_gen_len):
+    #         if self.attention:
+    #             decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden,encoder_outputs)
+    #         else:
+    #             decoder_output, decoder_hidden = self.decoder(decoder_input,decoder_hidden)
             
+    #         step_output = decoder_output.squeeze(1)
+    #         token_logits = self.generate_linear(step_output)
+    #         output = self.rule_filter_(output,token_logits)
             
     #         all_outputs.append(output)
     #         if self.share_vocab:
-    #             output=self.decode(output)
-    #             decoder_input=self.out_embedder(output)
+    #             output_=self.decode(output)
+    #             decoder_input=self.out_embedder(output_)
     #         else:
     #             decoder_input=self.out_embedder(output)
     #     all_outputs=torch.cat(all_outputs,dim=1)

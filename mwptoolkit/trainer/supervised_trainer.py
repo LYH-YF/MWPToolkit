03565a42f2bb7b18a9b98c6a1838f7c84be67914
LYH-YF <yifan2250@gmail.com>
2021-06-07 23:39:19 +0800

    Update supervised_trainer.py

--- mwptoolkit/trainer/supervised_trainer.py
+++ mwptoolkit/trainer/supervised_trainer.py
@@ -595,12 +595,12 @@ class TreeLSTMTrainer(AbstractTrainer):
         self.logger.info("start training...")
         for epo in range(self.start_epoch, epoch_nums):
             self.epoch_i = epo + 1
-            # self.model.train()
-            # loss_total, train_time_cost = self._train_epoch()
-            # self._scheduler_step()
+            self.model.train()
+            loss_total, train_time_cost = self._train_epoch()
+            self._scheduler_step()
 
-            # self.logger.info("epoch [%3d] avr loss [%2.8f] | train time %s"\
-            #                     %(self.epoch_i,loss_total/self.train_batch_nums,train_time_cost))
+            self.logger.info("epoch [%3d] avr loss [%2.8f] | train time %s"\
+                                %(self.epoch_i,loss_total/self.train_batch_nums,train_time_cost))
 
             if epo % self.test_step == 0 or epo > epoch_nums - 5:
                 if self.config["k_fold"]:

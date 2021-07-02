07687402d8b26aa9233bbde296cd8e3fc8e36050
LYH-YF <yifan2250@gmail.com>
2021-07-02 10:03:27 +0800

    Update supervised_trainer.py

--- mwptoolkit/trainer/supervised_trainer.py
+++ mwptoolkit/trainer/supervised_trainer.py
@@ -415,6 +415,32 @@ class GTSTrainer(AbstractTrainer):
         self.logger.info("test total [%d] | test equ acc [%2.3f] | test value acc [%2.3f] | test time %s"\
                                 %(eval_total,equation_ac/eval_total,value_ac/eval_total,test_time_cost))
 
+    def param_search(self):
+        train_batch_size = self.config["train_batch_size"]
+        epoch_nums = self.config["epoch_nums"]
+
+        self.train_batch_nums = int(self.dataloader.trainset_nums / train_batch_size) + 1
+
+        self.logger.info("start training...")
+        for epo in range(self.start_epoch, epoch_nums):
+            self.epoch_i = epo + 1
+            self.model.train()
+            loss_total, train_time_cost = self._train_epoch()
+            self._scheduler_step()
+            # self.logger.info("epoch [%3d] avr loss [%2.8f] | train time %s"\
+            #                     %(self.epoch_i,loss_total/self.train_batch_nums,train_time_cost))
+
+            if epo % self.test_step == 0 or epo > epoch_nums - 5:
+                valid_equ_ac, valid_val_ac, valid_total, valid_time_cost = self.evaluate(DatasetType.Valid)
+
+                # self.logger.info("---------- valid total [%d] | valid equ acc [%2.3f] | valid value acc [%2.3f] | valid time %s"\
+                #                 %(valid_total,valid_equ_ac,valid_val_ac,valid_time_cost))
+                test_equ_ac, test_val_ac, test_total, test_time_cost = self.evaluate(DatasetType.Test)
+
+                tune.report(accuracy=test_val_ac)
+
+                
+
 
 class MultiEncDecTrainer(GTSTrainer):
     def __init__(self, config, model, dataloader, evaluator):
@@ -728,6 +754,30 @@ class TreeLSTMTrainer(AbstractTrainer):
         test_time_cost = time_since(time.time() - test_start_time)
         self.logger.info("test total [%d] | test equ acc [%2.3f] | test value acc [%2.3f] | test time %s"\
                                 %(eval_total,equation_ac/eval_total,value_ac/eval_total,test_time_cost))
+
+    def param_search(self):
+        train_batch_size = self.config["train_batch_size"]
+        epoch_nums = self.config["epoch_nums"]
+
+        self.train_batch_nums = int(self.dataloader.trainset_nums / train_batch_size) + 1
+
+        self.logger.info("start training...")
+        for epo in range(self.start_epoch, epoch_nums):
+            self.epoch_i = epo + 1
+            self.model.train()
+            loss_total, train_time_cost = self._train_epoch()
+            self._scheduler_step()
+            # self.logger.info("epoch [%3d] avr loss [%2.8f] | train time %s"\
+            #                     %(self.epoch_i,loss_total/self.train_batch_nums,train_time_cost))
+
+            if epo % self.test_step == 0 or epo > epoch_nums - 5:
+                valid_equ_ac, valid_val_ac, valid_total, valid_time_cost = self.evaluate(DatasetType.Valid)
+
+                # self.logger.info("---------- valid total [%d] | valid equ acc [%2.3f] | valid value acc [%2.3f] | valid time %s"\
+                #                 %(valid_total,valid_equ_ac,valid_val_ac,valid_time_cost))
+                test_equ_ac, test_val_ac, test_total, test_time_cost = self.evaluate(DatasetType.Test)
+
+                tune.report(accuracy=test_val_ac)
 
 
 class SAUSolverTrainer(GTSTrainer):

12e689f0c4f17ff705025c606d7ccdb1afdf0073
LYH-YF <yifan2250@gmail.com>
2021-07-03 13:21:31 +0800

    Update supervised_trainer.py

--- mwptoolkit/trainer/supervised_trainer.py
+++ mwptoolkit/trainer/supervised_trainer.py
@@ -918,8 +918,8 @@ class TRNNTrainer(SupervisedTrainer):
             else:
                 equs_acc.append(False)
             if x:
-                self.logger('{} {} {}\n{} {} {}'.format(equ_out,temp_out,test_out,\
-                    equ_tar,temp_tar,target))
+                self.logger('{} {} {}\n{} {} {}'.format(equ_out[idx],temp_out[idx],test_out[idx],\
+                    equ_tar[idx],temp_tar[idx],target[idx]))
 
         return val_acc, equ_acc, temp_acc, equs_acc
 

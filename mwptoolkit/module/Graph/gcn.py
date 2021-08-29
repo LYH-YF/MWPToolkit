de450b4ff387d96b353f44c61b9b64be6c6c188c
LYH-YF <yifan2250@gmail.com>
2021-08-29 22:35:03 +0800

    graph

--- mwptoolkit/module/Graph/gcn.py
+++ mwptoolkit/module/Graph/gcn.py
@@ -1,23 +1,32 @@
+# -*- encoding: utf-8 -*-
+# @Author: Yihuai Lan
+# @Time: 2021/08/29 21:49:49
+# @File: gcn.py
+
+
 import torch
 from torch import nn
 from torch.nn import functional as F
 
 from mwptoolkit.module.Layer.graph_layers import GraphConvolution
+
+
 class GCN(nn.Module):
     def __init__(self, in_feat_dim, nhid, out_feat_dim, dropout):
         super(GCN, self).__init__()
-        '''
-        ## Inputs:
-        - graph_nodes (batch_size, K, in_feat_dim): input features
-        - adjacency matrix (batch_size, K, K)
-        ## Returns:
-        - gcn_enhance_feature (batch_size, K, out_feat_dim)
-        '''
         self.gc1 = GraphConvolution(in_feat_dim, nhid)
         self.gc2 = GraphConvolution(nhid, out_feat_dim)
         self.dropout = dropout
 
     def forward(self, x, adj):
+        """
+        Args:
+            x (torch.Tensor): input features, shape [batch_size, node_num, in_feat_dim]
+            adj (torch.Tensor): adjacency matrix, shape [batch_size, node_num, node_num]
+        
+        Returns:
+            torch.Tensor: gcn_enhance_feature, shape [batch_size, node_num, out_feat_dim]
+        """
         x = F.relu(self.gc1(x, adj))
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
--- mwptoolkit/module/Graph/graph_module.py
+++ mwptoolkit/module/Graph/graph_module.py
@@ -1,3 +1,9 @@
+# -*- encoding: utf-8 -*-
+# @Author: Yihuai Lan
+# @Time: 2021/08/29 22:00:28
+# @File: graph_module.py
+
+
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -8,47 +14,33 @@ from mwptoolkit.module.Graph.gcn import GCN
 class Graph_Module(nn.Module):
     def __init__(self, indim, hiddim, outdim, dropout=0.3):
         super(Graph_Module, self).__init__()
-        '''
-        ## Variables:
-        - indim: dimensionality of input node features
-        - hiddim: dimensionality of the joint hidden embedding
-        - outdim: dimensionality of the output node features
-        - combined_feature_dim: dimensionality of the joint hidden embedding for graph
-        - K: number of graph nodes/objects on the image
-        '''
+        """
+        Args:
+            indim: dimensionality of input node features
+            hiddim: dimensionality of the joint hidden embedding
+            outdim: dimensionality of the output node features
+            combined_feature_dim: dimensionality of the joint hidden embedding for graph
+            K: number of graph nodes/objects on the image
+        """
         self.in_dim = indim
-        #self.combined_dim = outdim
-        
-        #self.edge_layer_1 = nn.Linear(indim, outdim)
-        #self.edge_layer_2 = nn.Linear(outdim, outdim)
-        
-        #self.dropout = nn.Dropout(p=dropout)
-        #self.edge_layer_1 = nn.utils.weight_norm(self.edge_layer_1)
-        #self.edge_layer_2 = nn.utils.weight_norm(self.edge_layer_2)
         self.h = 4
         self.d_k = outdim//self.h
         
-        #layer = GCN(indim, hiddim, self.d_k, dropout)
         self.graph = nn.ModuleList()
         for _ in range(self.h):
             self.graph.append(GCN(indim,hiddim,self.d_k,dropout))
-        #self.graph = clones(GCN(indim, hiddim, self.d_k, dropout), 4)
-        
-        #self.Graph_0 = GCN(indim, hiddim, outdim//4, dropout)
-        #self.Graph_1 = GCN(indim, hiddim, outdim//4, dropout)
-        #self.Graph_2 = GCN(indim, hiddim, outdim//4, dropout)
-        #self.Graph_3 = GCN(indim, hiddim, outdim//4, dropout)
         
         self.feed_foward = PositionwiseFeedForward(indim, hiddim, outdim, dropout)
         self.norm = LayerNorm(outdim)
 
     def get_adj(self, graph_nodes):
-        '''
-        ## Inputs:
-        - graph_nodes (batch_size, K, in_feat_dim): input features
-        ## Returns:
-        - adjacency matrix (batch_size, K, K)
-        '''
+        """
+        Args:
+            graph_nodes (torch.Tensor): input features, shape [batch_size, node_num, in_feat_dim]
+        
+        Returns:
+            torch.Tensor: adjacency matrix, shape [batch_size, node_num, node_num]
+        """
         self.K = graph_nodes.size(1)
         graph_nodes = graph_nodes.contiguous().view(-1, self.in_dim)
         
@@ -69,12 +61,13 @@ class Graph_Module(nn.Module):
         return adjacency_matrix
     
     def normalize(self, A, symmetric=True):
-        '''
-        ## Inputs:
-        - adjacency matrix (K, K) : A
-        ## Returns:
-        - adjacency matrix (K, K) 
-        '''
+        """
+        Args:
+            A (torch.Tensor): adjacency matrix (node_num, node_num)
+        
+        Returns:
+            adjacency matrix (node_num, node_num) 
+        """
         A = A + torch.eye(A.size(0)).cuda().float()
         d = A.sum(1)
         if symmetric:
@@ -92,40 +85,28 @@ class Graph_Module(nn.Module):
         return adj
 
     def forward(self, graph_nodes, graph):
-        '''
-        ## Inputs:
-        - graph_nodes (batch_size, K, in_feat_dim): input features
-        ## Returns:
-        - graph_encode_features (batch_size, K, out_feat_dim)
-        '''
+        """
+        Args:
+            graph_nodes (torch.Tensor):input features, shape [batch_size, node_num, in_feat_dim]
+        
+        Returns:
+            torch.Tensor: graph_encode_features, shape [batch_size, node_num, out_feat_dim]
+        """
         nbatches = graph_nodes.size(0)
         mbatches = graph.size(0)
         if nbatches != mbatches:
             graph_nodes = graph_nodes.transpose(0, 1)
-        # adj (batch_size, K, K): adjacency matrix
         if not bool(graph.numel()):
             adj = self.get_adj(graph_nodes)
-            #adj = adj.unsqueeze(1)
-            #adj = torch.cat((adj,adj,adj),1)
             adj_list = [adj,adj,adj,adj]
         else:
             adj = graph.float()
             adj_list = [adj[:,1,:],adj[:,1,:],adj[:,4,:],adj[:,4,:]]
-        #print(adj)
-        
+
         g_feature = \
             tuple([l(graph_nodes,x) for l, x in zip(self.graph,adj_list)])
-        #g_feature_0 = self.Graph_0(graph_nodes,adj[0])
-        #g_feature_1 = self.Graph_1(graph_nodes,adj[1])
-        #g_feature_2 = self.Graph_2(graph_nodes,adj[2])
-        #g_feature_3 = self.Graph_3(graph_nodes,adj[3])
-        #print('g_feature')
-        #print(type(g_feature))
-        
         
         g_feature = self.norm(torch.cat(g_feature,2)) + graph_nodes
-        #print('g_feature')
-        #print(g_feature.shape)
         
         graph_encode_features = self.feed_foward(g_feature) + g_feature
         

5cc0351039f61795ecdc8417c4eb76c41a09b8b8
LYH-YF <yifan2250@gmail.com>
2021-09-02 19:47:17 +0800

    v0.0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 import setuptools
 
-install_requires = ["torch>=1.5.0", "transformers==4.3.3", "stanza>=1.2", "sympy>=1.6", "ray>=1.3.0", "nltk>=3.5", "gensim>=4.0.1", "word2number>=1.1"]
+install_requires = ["torch>=1.5.0", "transformers==4.3.3", "stanza>=1.2", "sympy>=1.6", "ray>=1.3.0", "nltk>=3.5", "gensim>=4.0.1", "word2number>=1.1","ray[tune]"]
 
 classifiers = [
     'Operating System :: OS Independent', 'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7',
@@ -16,7 +16,7 @@ with open("PYPI.md", "r") as f:
 
 setuptools.setup(
     name='mwptoolkit',
-    version='0.0.1',
+    version='0.0.4',
     url="https://github.com/LYH-YF/MWPToolkit",
     author=" ",
     long_description=long_description,
